cdc19c3fa579aad744d4132038a1b2d7e8c60587
Abby Wagoner <awagoner05@icloud.com>
2020-10-25 11:09:42 -0500

    Creating and Using a Persistent Controller

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,17 +1,15 @@
 package org.launchcode.codingevents.controllers;
 
-import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -20,10 +18,15 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    @Autowired
+    private EventRepository eventRepository;
+
+    //findAll, save, findById
+
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
 
         return "events/index";
     }
@@ -43,14 +46,14 @@ public class EventController {
             model.addAttribute("title", "Create Event");
             return "events/create";
         }
-        EventData.add(newEvent);
+        eventRepository.save(newEvent);
         return "redirect:";
     }
 
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
 
@@ -58,31 +61,31 @@ public class EventController {
     public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
         if (eventIds != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                eventRepository.deleteById(id);
             }
         }
         return "redirect:";
     }
 
-    @GetMapping("edit/{eventId}")
-    public String displayEditForm(Model model, @PathVariable int eventId) {
-        //controller code will go here
-        Event event = EventData.getByID(eventId);
-        model.addAttribute("event", event);
-        if (event != null) {
-            model.addAttribute("title", "Edit Event " + event.getName() + " (ID = " + event.getId() + ")");
-        }
-
-        return "events/edit";
-    }
-
-    @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description) {
-        Event event = EventData.getByID(eventId);
-        event.setName(name);
-        event.setDescription(description);
-        return "redirect:";
-    }
+//    @GetMapping("edit/{eventId}")
+//    public String displayEditForm(Model model, @PathVariable int eventId) {
+//        //controller code will go here
+//        Event event = EventData.getByID(eventId);
+//        model.addAttribute("event", event);
+//        if (event != null) {
+//            model.addAttribute("title", "Edit Event " + event.getName() + " (ID = " + event.getId() + ")");
+//        }
+//
+//        return "events/edit";
+//    }
+//
+//    @PostMapping("edit")
+//    public String processEditForm(int eventId, String name, String description) {
+//        Event event = EventData.getByID(eventId);
+//        event.setName(name);
+//        event.setDescription(description);
+//        return "redirect:";
+//    }
 
 
 }
